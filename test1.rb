3e810845f8a4b24a7c34d37f4a12bd45a9348ed2
OleksiiVlasenko <a.a.a.vlasenko@gmail.com>
2020-11-12 09:19:05 +0200

    test

--- test1.rb
+++ test1.rb
@@ -1,5 +1,12 @@
-var1 = "testit"
-p var1.reverse
-p var1
-p var1.reverse!
-p var1
+# var1 = "testit"
+# p var1.reverse
+# p var1
+# p var1.reverse!
+# p var1
+letters = 'aAbBcCdDeE'
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts ' a'.capitalize
+puts letters
