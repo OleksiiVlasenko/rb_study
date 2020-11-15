019c1f12ef144dc3f15b6fed3e4f3b921b77a39f
OleksiiVlasenko <a.a.a.vlasenko@gmail.com>
2020-11-15 21:15:06 +0200

    study

--- test1.rb
+++ test1.rb
@@ -16,12 +16,20 @@
 #   def sum(n)
 #     n += n
 #   end
-# end
+# # end
+#
+# arr = Array.new(10) { rand(1..9) }
+# p arr
+#   p arr.map(&:odd?)
+# p arr.map(&:real?)
+# # p [*1..5].map(&:sum)
+# p 122345.digits
+# p 10.step(by: +1).take(4)
+#
+def explictic(&block)
+  return  "no block" if block.nil?
+  block.call
+end
 
-arr = Array.new(10) { rand(1..9) }
-p arr
-  p arr.map(&:odd?)
-p arr.map(&:real?)
-# p [*1..5].map(&:sum)
-p 122345.digits
-p 10.step(by: +1).take(4)
+explictic { p '2'}
+explictic
