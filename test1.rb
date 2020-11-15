49e511f39eefb0e6189fecf25937e83610c82c89
OleksiiVlasenko <a.a.a.vlasenko@gmail.com>
2020-11-15 17:44:45 +0200

    study

--- test1.rb
+++ test1.rb
@@ -2,11 +2,15 @@
 # p var1.reverse
 # p var1
 # p var1.reverse!
-# p var1
-letters = 'aAbBcCdDeE'
-puts letters.upcase
-puts letters.downcase
-puts letters.swapcase
-puts letters.capitalize
-puts ' a'.capitalize
-puts letters
+# # p var1
+# letters = 'aAbBcCdDeE'
+# puts letters.upcase
+# puts letters.downcase
+# puts letters.swapcase
+# puts letters.capitalize
+# puts ' a'.capitalize
+# puts letters
+#
+arr = Array.new(10) { rand(1..9) }
+p arr
+  p arr.map(&:odd?)
